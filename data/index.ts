d466ade52824658c6f4f77317472742dad59828c
user.name: "Krishna Singh <user.email: "krishnasingh2007m@gmail.com>
2025-03-11 03:08:36 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+  { name: "CV", link: "https://chocolate-maura-73.tiiny.site" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Exploring the blueprint for the world's first machine-powered brain.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "HooBank - Modern Bank Landing Page",
+    des: "A modern bank landing page with a clean and minimalistic design, with best ui/ux practices.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://hoo-bank-web-app-five.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://yoom-video-meeting-hazel.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "CarePulse - Appointment Scheduling System",
+    des: "A user-friendly appointment scheduling system for healthcare providers",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://krish-care-pulse.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://iphone-clone-rouge.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Krish is an incredible asset to Karmyug Tech Zone. His technical skills, innovative mindset, and dedication to delivering high-quality solutions have been instrumental in our growth. As a partner, his contributions have been invaluable, especially in developing projects that align perfectly with our company vision. His ability to adapt and execute complex tasks with precision is a testament to his expertise. I am proud to work alongside someone as talented and driven as Krish, and I am excited to see what more we can achieve together in the future.",
+    name: "Krishna Singh Devda",
+    title: "CEO, KarmyugTechzone",
+  },
+  {
+    quote:
+      "Collaborating with Krish was a game-changer for our project. His ability to blend creativity with functionality was beyond impressive. From understanding our requirements to delivering a flawless solution, Krish showcased professionalism and expertise every step of the way. His dedication to ensuring the best outcomes made working with him a truly rewarding experience.",
+    name: "Amit Patel,",
+    title: "Freelance Graphic Designer",
+  },
+  {
+    quote:
+      "Krish has truly impressed us with his outstanding work on the website for St. Stephen's School. As a student, his ability to create such a professional and efficient platform speaks volumes about his talent and dedication. The website is user-friendly, visually appealing, and meets all our needs perfectly. His commitment to excellence at such a young age is something we take immense pride in. We are proud to have him as part of our school and look forward to seeing him achieve even greater success in the future.",
+    name: "Sanjay Upadhyay",
+    title: "Director St. Stephen's School",
+  },
+  {
+    quote:
+      "It’s rare to find someone like Krish, who combines technical expertise with a genuine enthusiasm for creating impactful solutions. His meticulous approach to problem-solving and his willingness to go the extra mile ensured our project was a success. Krish is not just a great collaborator but also a true visionary.",
+    name: "Rohan Desai",
+    title: "Small Business Owner",
+  },
+  {
+    quote:
+      "Krish's work on developing the Next.js application for my online course was exceptional. From the initial consultation to the final delivery, he demonstrated not only his technical expertise but also a deep understanding of my needs. The application is intuitive, efficient, and perfectly aligned with the course content. His professionalism and commitment to delivering high-quality results have made this collaboration truly memorable. I highly recommend Krish to anyone looking for innovative solutions and reliable support.",
+    name: "Acharya Mohan Prajapat",
+    title: "Vastu Specialist",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    linksocial: "https://github.com/KrishTechverse",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    linksocial: "https://x.com/Krish_byte?t=hOvty9hO0jBf2H9WyxVTxQ&s=09",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    linksocial: "https://www.linkedin.com/in/krishna-singh-5b4402253/",
+  },
+];
